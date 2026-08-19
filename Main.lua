9cad4704eddaafa5d8bd7bede53b59239865bb94
gokuhub-hub <gokuhub401@gmail.com>
2026-08-19 14:29:53 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,37 +1,19 @@
-local Scripts = {
-    = {
-        Name = "Block Spin",
-        Load = function()
+local goku_hub = {
+    [6765805766] = {
+        Nombre = "Block Spin",
+        Cargar = function()
            loadstring(game:HttpGet("https://jnkie.com"))()
         end
-    },
+    }
 }
 
-local Goku = Scripts[game.GameId]
-
-if not Goku then
-    game.Players.LocalPlayer:Kick("Game not supported")
+if not goku_hub[game.PlaceId] then
+    game.Players.LocalPlayer:Kick("Goku Hub: Juego no compatible")
     return
 end
 
-print("Loaded for " .. Goku.Name)
+print("Goku Hub cargado con éxito para " .. goku_hub[game.PlaceId].Nombre)
 
 pcall(function()
-    Goku.Load()
+    goku_hub[game.PlaceId].Cargar()
 end)
-
-local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
-local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "GokuGui"
-screenGui.ResetOnSpawn = false
-screenGui.Parent = playerGui
-
-local imageLabel = Instance.new("ImageLabel")
-imageLabel.Name = "GokuImage"
-imageLabel.Size = UDim2.new(0, 150, 0, 150)
-imageLabel.Position = UDim2.new(1, -160, 1, -160)
-imageLabel.BackgroundTransparency = 1
-imageLabel.Image = "rbxassetid://10056401344"
-imageLabel.Parent = screenGui
